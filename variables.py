41509584cb787928429ef7235e30a592be1541a6
asdfghjkl1237 <kov.tangome@gmail.com>
2024-08-10 02:06:10 +0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -11,3 +11,5 @@ path_folder_octavia_core = os.path.dirname(path_folder_oct)
 
 path_folder__test_sound_listen = os.path.join(path_folder_octavia_core, 'storage', 'test', 'sound_listen')
 path_folder__test_sound_gen = os.path.join(path_folder_octavia_core, 'storage', 'test', 'sound_gen')
+
+del_samle_size_multiplier = 1000
