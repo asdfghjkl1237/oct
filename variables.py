67d1037ebe578bc6e9831727a459c5a9d4d8f627
asdfghjkl1237 <kov.tangome@gmail.com>
2024-08-10 01:41:26 +0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -5,7 +5,7 @@ chunk = 1024
 channels = 1
 bit_duration = 0.001
 
-path_folder_oct = os.path.abspath(__file__)
+path_folder_oct = os.path.dirname(os.path.abspath(__file__))
 path_folder_octavia_core = os.path.dirname(path_folder_oct)
 
 path_folder__test_sound_listen = os.path.join(path_folder_octavia_core, 'storage', 'test', 'sound_listen')
