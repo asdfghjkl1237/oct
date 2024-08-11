6d546fcb0df641c9fcbf40fe72e0d6908df675ef
asdfghjkl1237 <kov.tangome@gmail.com>
2024-08-11 19:08:48 +0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -12,7 +12,7 @@ path_folder_octavia_core = os.path.dirname(path_folder_oct)
 path_folder__test_sound_listen = os.path.join(path_folder_octavia_core, 'storage', 'test', 'sound_listen')
 path_folder__test_sound_gen = os.path.join(path_folder_octavia_core, 'storage', 'test', 'sound_gen')
 
-del_sample_size_multiplier = 100
+del_sample_size_multiplier = 100*4
 
 dict_frequencies = {0: 1000, 1: 3000}
 dict_frequencies_reverse = {v: k for k, v in dict_frequencies.items()}
