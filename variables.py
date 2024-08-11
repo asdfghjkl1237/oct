91d3b5436aac8beadb804f539b5ce7a015764456
asdfghjkl1237 <kov.tangome@gmail.com>
2024-08-11 19:13:53 +0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -3,7 +3,7 @@ from packages import *
 framerate = 96000 # 44100
 chunk = 1024
 channels = 1
-bit_duration = 0.005
+bit_duration = 0.0025
 primary_recording_id = 9
 
 path_folder_oct = os.path.dirname(os.path.abspath(__file__))
@@ -12,7 +12,7 @@ path_folder_octavia_core = os.path.dirname(path_folder_oct)
 path_folder__test_sound_listen = os.path.join(path_folder_octavia_core, 'storage', 'test', 'sound_listen')
 path_folder__test_sound_gen = os.path.join(path_folder_octavia_core, 'storage', 'test', 'sound_gen')
 
-del_sample_size_multiplier = 100*4
+del_sample_size_multiplier = 100*4*2
 
 dict_frequencies = {0: 1000, 1: 3000}
 dict_frequencies_reverse = {v: k for k, v in dict_frequencies.items()}
